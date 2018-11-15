ba61875a709243723d3fb947b73987e5fc4ef884
AleksandarDatsov <a.datsov@gmail>
2018-11-15 17:41:28 +0200

    FutureAgeCalculator added

---
+++ src/main/java/com/musala/javacourse181112/FutureAgeCalculator.java
@@ -0,0 +1,18 @@
+package com.musala.javacourse181112;
+
+public class FutureAgeCalculator {
+    public static void main(String[] args) {
+
+        int result = calculate(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+
+        System.out.println("Your current age is: " + args[0] + " years and after " + args[1] +
+                " years you will be " + result + " years old");
+
+        System.exit(0);
+    }
+
+    public static int calculate (int a, int b) {
+        return a+b;
+    }
+
+}
