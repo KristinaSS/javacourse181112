4cd6be5751be832f915ae99d5f48b749c507d111
AleksandarDatsov <a.datsov@gmail>
2018-11-15 19:17:08 +0200

    operatorsExample added

---
+++ src/main/java/com/musala/javacourse181112/OperatorsExample.java
@@ -0,0 +1,28 @@
+package com.musala.javacourse181112;
+
+public class OperatorsExample {
+    public static void main(final String[] args) {
+        int i1 = 0;
+        //int i2 = 1;
+
+        i1 = 0;
+        i1 += 1;
+        System.out.println(i1);
+
+        i1 = 0;
+        i1 = i1 * 2;
+        i1 *= 2;
+        System.out.println(i1++);
+
+        System.out.println(true);
+        System.out.println(!true);
+
+        System.out.println(2 * 2);
+        System.out.println(4 / 2);
+        System.out.println(4 % 3);
+
+        String value = "asd";
+        String value1 = args[1];
+//        System.out.println(value == value1);
+    }
+}
