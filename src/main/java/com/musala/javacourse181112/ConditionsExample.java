7f625bb6f2366c87591d3156b9f86c89cfd2002b
bomogilanski <bomogilanski@yahoo.com>
2018-11-19 17:39:51 +0200

    Cex

---
+++ src/main/java/com/musala/javacourse181112/ConditionsExample.java
@@ -0,0 +1,16 @@
+package com.musala.javacourse181112;
+
+public class ConditionsExample {
+    public static void main(String[] args) {
+        if (2 > 2) {
+            System.out.println("true");
+        } else if (5 > 6) {
+            System.out.println("0");
+        } else if (6 > 7) {
+            System.out.println("1");
+        } else if (7 == 7) {
+            System.out.println("2");
+        }
+    }
+
+}
