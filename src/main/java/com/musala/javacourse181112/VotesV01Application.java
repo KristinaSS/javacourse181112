4062f809263d1b1f255e4f98a6d21872e69ff863
AleksandarDatsov <a.datsov@gmail>
2018-11-19 13:23:53 +0200

    Votes 0.1 application - output edited

--- src/main/java/com/musala/javacourse181112/VotesV01Application.java
+++ src/main/java/com/musala/javacourse181112/VotesV01Application.java
@@ -4,10 +4,6 @@ package com.musala.javacourse181112;
 public class VotesV01Application {
     public static void main(final String[] args) {
 
-        System.out.println("Welcome! Question 1: Is it snowing today?");
-        System.out.println("Welcome! Question 2: Do you rather be snowing?");
-
-
         boolean[][] answer = new boolean[2][30];
         answer[0][0] = false;
         answer[0][1] = false;
@@ -16,10 +12,10 @@ public class VotesV01Application {
         answer[1][1] = false;
         answer[1][2] = true;
 
-        System.out.println("Question1: \nVoter 1 answers: " + answer[0][0]);
+        System.out.println("Welcome! Question 1: Is it snowing today? \nVoter 1 answers: " + answer[0][0]);
         System.out.println("Voter 2 answers: " + answer[0][1]);
         System.out.println("Voter 3 answers: " + answer[0][2]);
-        System.out.println("Question2: \nVoter 1 answers: " + answer[1][0]);
+        System.out.println("Welcome! Question 2: Do you rather be snowing? \nVoter 1 answers: " + answer[1][0]);
         System.out.println("Voter 2 answers: " + answer[1][1]);
         System.out.println("Voter 1 answers: " + answer[1][2]);
 
