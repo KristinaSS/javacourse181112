2a4d4b8bd692fcf359c718728f4eb6400cf8fd75
bomogilanski <bomogilanski@yahoo.com>
2018-11-19 19:12:53 +0200

    chrmApp

--- src/main/java/com/musala/javacourse181112/tasks/chromosomesApp.java
+++ src/main/java/com/musala/javacourse181112/tasks/chromosomesApp.java
@@ -1,8 +1,24 @@
 package com.musala.javacourse181112.tasks;
 
+import java.util.Scanner;
+
 public class chromosomesApp {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        String gender = sc.next();
+        if (gender.equals("male")) {
+            System.out.println("male: yx");
+        } else if (gender.equals("female")){
+            System.out.println("female: xx");
+        }
+        switch (gender) {
+            case "male":
+                System.out.println("Y");
+                break;
+            case "female":
+                System.out.println("X");
+        }
     }
-    }
+}
 
