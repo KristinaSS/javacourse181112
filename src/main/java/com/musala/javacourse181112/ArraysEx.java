060f9a83a33aca591c272afaef8f02cb2bd04c18
bomogilanski <bomogilanski@yahoo.com>
2018-11-19 17:40:06 +0200

    ArrEx

---
+++ src/main/java/com/musala/javacourse181112/ArraysEx.java
@@ -0,0 +1,13 @@
+package com.musala.javacourse181112;
+
+public class ArraysEx {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        //intArray = new int[intArray.length+3];
+        //intArray = new int[13];
+        intArray[0] = 1;
+        intArray[9] = 15;
+        System.out.println(intArray);
+        long[] longArray = {1L, 4L, 5L};
+    }
+}
